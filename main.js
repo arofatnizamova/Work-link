53a5ae4425c49d38943926ac8afc3443913e032f
Your Name <you@example.com>
2025-05-16 17:09:39 +0500

    validates pages

--- main.js
+++ main.js
@@ -15,20 +15,43 @@ $(document).ready(function() {
 
         if (slider.hasClass('image-slider')) {
             extraOptions = {
-                focus: 1,
+                focus: 2,
                 interval: 4000,
                 pagination: false,
                 arrows: false,
-                perPage: 3,
+                perPage: 5,
+                grid       : {
+                    dimensions: [ [ 2, 1 ], [ 2, 1 ], [ 1, 1 ], [ 2, 1 ], [ 1, 1], [ 1, 1 ] ],
+                },
+                breakpoints: {
+                    576: {
+                      perPage: 1,
+                      grid   : {
+                        dimensions: [ [ 1]],
+                      },
+                    },
+                  },
             };
-        } 
+        } else if (slider.hasClass('image-slider-mobile')){
+            extraOptions = {
+                perPage: 2.3,
+                gap: '20px',
+                pagination: false,
+                arrows: false,
+                breakpoints: {
+                    768: {
+                      perPage: 1,
+                    },
+                  },
+            };
+        }
         const finalOptions = {...options, ...extraOptions };
         const splide = new Splide(slider[0], finalOptions);
 
         if (prevBtn) prevBtn.addEventListener('click', () => splide.go('-1'));
         if (nextBtn) nextBtn.addEventListener('click', () => splide.go('+1'));
 
-        splide.mount();
+        splide.mount(window.splide.Extensions);
     });
     AOS.init({
         duration: 800,
